3a3b3ea6c61ef12c29e71156cd7d0dbec983c1ad
AbhishekHulule9579 <abhishekhulule.sknsits.it@gmail.com>
2026-01-12 18:58:50 +0530

    Add array manipulation methods: pop, shift, splice, map, filter, reduce, and values

--- starts/J2_Array.js
+++ starts/J2_Array.js
@@ -41,3 +41,51 @@ c.unshift(10,20)
 console.log(c)
 
 console.log("************ Array Pop method ************")
+let d=[10,20,30,40,50]
+d.pop()
+console.log(d)
+
+console.log("************ Array shift method ****************")
+let d1=[10,20,30,40,50]
+d1.shift();
+console.log(d1)
+
+console.log("******************* Array Splice ******************")
+let d2=[10,20,30,40,50]
+d2.splice(1,3)
+console.log(d2)
+d2.splice(2,4,5,67,8,9,3)
+console.log(d2)
+
+console.log("******************Array Map method ************")
+let e=[4,9,16,25];
+let sub=e.map(squart);
+function squart(){
+    return e.map(Math.sqrt);
+}
+console.log(sub);
+
+console.log(" ******************* Array Filter Method ***************")
+let f=[1,2,3,4,5,6];
+let f1=f.filter((num)=>num>2);
+console.log(f1)
+
+console.log("*************** Array Reduce Method ************")
+let g=[99,50,45,6,3,1];
+let subb=g.reduce(geeks)
+function geeks(tot,num){
+    return tot-num;
+}
+console.log(subb)
+
+console.log("***************Array Reduce Method *****************")
+let h=[1,2,3,4,5,6,7,8,9]
+h.reverse();
+console.log(h)
+
+console.log("************** Array values() method ************")
+const k=["Apple","Moto","Blackberry","Samsung"];
+const ress=k.values();
+for(const value of ress){
+    console.log(value)
+}
