506c2dfd5cdcd644b86e8e6cdf672d785e028992
AbhishekHulule9579 <abhishekhulule.sknsits.it@gmail.com>
2026-01-30 23:02:56 +0530

    Add string declaration examples using single quotes, double quotes, and constructor

---
+++ starts/J3_String.js
@@ -0,0 +1,11 @@
+
+console.log("************ String declaration ****************")
+console.log( "********* by single and double inverted commas" )
+let s1='abc';
+console.log(s1)
+let s2="abc"
+console.log(s2)
+
+console.log("*********declaration by using constructor *************")
+let s3=new String('abcd')
+console.log(s3)
