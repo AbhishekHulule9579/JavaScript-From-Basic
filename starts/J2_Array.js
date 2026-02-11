99ff4f39f87b413a3ab8473c9b543e61298deb5a
Abhishek Hulule <156299375+AbhishekHulule9579@users.noreply.github.com>
2026-02-11 22:25:10 +0530

    Enhance J2_Array.js with array method examples
    Added examples for array methods including concat, flat, push, and unshift.

--- starts/J2_Array.js
+++ starts/J2_Array.js
@@ -14,6 +14,7 @@ let emp={
     salary:"50000",
     department:"Software"
 }
+
 console.log(delete emp.salary)
 console.log(emp)
 
@@ -25,17 +26,20 @@ let newarr=a1.concat(a2,a3)
 console.log(newarr)
 
 console.log("************ Array Flat ************")
+
 const a4=[[1,2,3],4,[5,6,7,8],9,10,[11,12,13]]
 const a5=a4.flat(Infinity)
 console.log(a5)
 
 console.log("************ Array Push method ************")
+
 const a6=[10,20,30,40]
 a6.push(50,60);
 a6.push(70)
 console.log(a6)
 
 console.log("************ Array unshift method ************")
+
 let c=[20,30,40]
 c.unshift(10,20)
 console.log(c)
